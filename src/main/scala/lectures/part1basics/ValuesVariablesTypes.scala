1fbda9c58b3f9a9209d3c60171a926c3a3ae90b0
pp-zbigz <pasuk.phonsuphee@gmail.com>
2022-12-10 04:29:21 +0700

    add ValuesVariablesSession

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,19 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  var x = 2422
+  x = 2
+  println(x)
+  //VALS ARE IMMUTABLES
+  //VARS ARE MUTABLES
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 54235423L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+}
